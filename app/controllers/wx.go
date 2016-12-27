c51e10316f64661eee986e26f22ba04d4bb9c61f
陈召东 <zhaodong@staff.sina.com.cn>
2016-12-27 19:12:20 +0800

    add

--- app/controllers/wx.go
+++ app/controllers/wx.go
@@ -57,9 +57,9 @@ func (c WxApp) WxP() revel.Result {
 
 func (c WxApp) Index() revel.Result {
 	user := c.wxId()
-	revel.INFO.Println("WxApp/Index: " + user.Name + " " + c.Request.UserAgent())
-
-	if user == nil {
+	if user != nil {
+		revel.INFO.Println("WxApp/Index: " + user.Name + " " + c.Request.UserAgent())
+	} else {
 		c.Redirect("https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx520c15f417810387&redirect_uri=https%3A%2F%2Fchong.qq.com%2Fphp%2Findex.php%3Fd%3D%26c%3DwxAdapter%26m%3DmobileDeal%26showwxpaytitle%3D1%26vb2ctag%3D4_2030_5_1194_60&response_type=code&scope=snsapi_base&state=123#wechat_redirect")
 	}
 	//获取boxlist 商品
